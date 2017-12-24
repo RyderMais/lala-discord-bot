eb362cd07c272f5998ba9a25e92ef711fa57dade
RyderMais <rydermais@gmail.com>
2017-12-24 07:13:42 -0200

    Update init.js

--- init.js
+++ init.js
@@ -1,14 +1,33 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({
+  commandPrefix: 'DISCORDBOT_PREFIX',
+  unknownCommandResponse: false,
+  owner: '269803975090372608',
+  disableEveryone: true
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['other', 'Comandos diversificados']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log('Bot inicializado!');
+  client.user.setGame('Love Ritmo Brasil');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
 });
+if (command === "ping") {
+  const m = await message.channel.send("Ping?");
+  m.edit(`Pong! A mensagem chegou em ${m.createdTimestamp - message.createdTimestamp}ms (sendo ${Math.round(client.ping)}ms de processamento e retorno.)`);
+}
 
 client.login(process.env.DISCORDBOT_TOKEN);
