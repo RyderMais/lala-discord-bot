f1c2c4e562dad4e754fb59875f6fbc6bdf46b2e8
RyderMais <rydermais@gmail.com>
2017-12-24 08:25:31 -0200

    Update init.js

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({
-  commandPrefix: DISCORDBOT_PREFIX,
+  commandPrefix: process.env.DISCORDBOT_PREFIX,
   unknownCommandResponse: false,
   owner: '269803975090372608',
   disableEveryone: true
