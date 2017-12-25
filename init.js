32d82e63b93a621b7fb09558cc7b8312133d4efe
RyderMais <rydermais@gmail.com>
2017-12-25 05:38:42 -0200

    Update init.js

--- init.js
+++ init.js
@@ -1,10 +1,6 @@
 // Global Variables
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const http = require('http');
-const express = require('express');
-const app = express();
-const port = process.env.DISCORDBOT_WEBPORT;
 
 
 // Connect
@@ -22,23 +18,5 @@ client.login(process.env.DISCORDBOT_TOKEN);
 
 // Commands and Responses
 client.on('message', msg => {
-  if (!msg.content.startsWith(process.env.DISCORDBOT_PREFIX) || !msg.guild) return;
-  const command = msg.content.split(' ')[0].substr(process.env.DISCORDBOT_PREFIX.length);
-  const args = msg.content.split(' ').slice(1).join(' ');
-  if (command === 'versão' || 'version') return msg.channel.send(process.env.DISCORDBOT_VERSION);
-  else if (command === 'convite' || 'invite') return msg.channel.send(process.env.DISCORDBOT_INVITE);
   if (msg.content === 'meu avatar') return msg.reply(message.author.avatarURL);
 });
-
-// Wake up, bot!
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.get('/', (request, response) => {
-    response.render('index');
-});
-app.listen(port, () => {
-    console.log('App funcionando em [http://localhost:' + port + '].');
-});
-setInterval(() => {
-  http.get('http://lala-discordbot.herokuapp.com');
-}, 580000);
